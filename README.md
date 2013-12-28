a8130fb59ca50f4711ef9f8d804f72c909690700
omphalos <omph.alos@yahoo.com>
2013-12-28 10:14:14 -0600

    Update IE support version.  Handle IE<10 better.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 traceStack
 ==========
 
-This is a simple micro library for generating a consistent cross-browser object model for stack trace objects.  Less than 600 bytes minified, it supports IE8+, Firefox, Chrome, and Safari.
+This is a simple micro library for generating a consistent cross-browser object model for stack trace objects.  Less than 600 bytes minified, it supports IE10+, Firefox, Chrome, and Safari.
 
 License
 =======
--- traceStack.js
+++ traceStack.js
@@ -1,30 +1,30 @@
-(function() { 
+;(function() {
 
   function parse(s) {
 
     if(s[s.length - 1] == ')') { // IE8+, top frame of Chrome
-    
+
       var firstParenIndex = s.indexOf('(')
         , lastParenIndex = s.indexOf(')')
         , inParens = s.substring(firstParenIndex + 1, lastParenIndex)
         , parts = inParens.split(':')
         , len = parts.length
-  
+
       return {
         line: +parts[len - 2],
         script: parts.slice(0, len - 2).join(':')
       }
     }
-    
+
     var lastColonIndex = s.lastIndexOf(':')
       , afterLastColon = s.substring(lastColonIndex + 1)
       , beforeLastColon = s.substring(0, lastColonIndex)
-    
+
     if(s[0] == '@') // Firefox
       return { line: +afterLastColon, script: beforeLastColon.substring(1) }
 
     if(s.indexOf('    at') == 0) { // Chrome 
-      // Chrome
+
       var secondToLastColonIndex = beforeLastColon.lastIndexOf(':')
         , afterSecondToLastColon = beforeLastColon.substring(secondToLastColonIndex + 1)
         , beforeSecondToLastColon = beforeLastColon.substring(7, secondToLastColonIndex)
@@ -41,11 +41,11 @@
       throw new Error()
     } catch(e) { 
 
-      return e.stack.
+      return (e.stack || '').
         split('\n').
         filter(function(x) { return x && x != 'Error' }).
         map(parse)
     }
   }
 
-})(this)
+})(this);
